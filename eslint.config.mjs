d134760b1a3fb857d334c2a842c66e2f61f6727b
Higinio <higinio.sosa.isc@gmail.com>
2025-09-29 21:21:53 -0600

    refactor(eslint): ajustar configuración de ESLint y cambiar comando de lint en package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,19 +9,18 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    rules: {
-      'prefer-const': 'error',
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['error'],
-      'no-console': 'warn',
-      'react-hooks/exhaustive-deps': 'warn',
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-img-element': 'error',
-    },
+const eslintConfig = [{
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}, ...compat.extends('next/core-web-vitals', 'next/typescript'), {
+  rules: {
+    'prefer-const': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'no-console': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/no-unescaped-entities': 'off',
+    '@next/next/no-img-element': 'error',
   },
-]
+}]
 
 export default eslintConfig
