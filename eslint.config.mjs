cf1095582244699bd5b0785bb88a222230e4d138
Higinio <higinio.sosa.isc@gmail.com>
2025-09-29 21:22:47 -0600

    refactor(eslint): mejorar la estructura de la configuración de ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,18 +9,28 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-const eslintConfig = [{
-  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}, ...compat.extends('next/core-web-vitals', 'next/typescript'), {
-  rules: {
-    'prefer-const': 'error',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['error'],
-    'no-console': 'warn',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react/no-unescaped-entities': 'off',
-    '@next/next/no-img-element': 'error',
+const eslintConfig = [
+  {
+    ignores: [
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+    ],
   },
-}]
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      'prefer-const': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error'],
+      'no-console': 'warn',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-img-element': 'error',
+    },
+  },
+]
 
 export default eslintConfig
